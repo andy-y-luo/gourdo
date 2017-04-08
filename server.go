ca39dcaff5ba3507bf74d612a91c557640a93d18
Eric Pauley <eric@pauley.me>
2017-04-08 17:09:21 -0400

    Added server

---
+++ server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/websocket"
+
+	"github.com/tjgq/broadcast"
+)
+
+var b broadcast.Broadcaster
+
+type packet struct {
+	Op   byte
+	Data int32
+}
+
+func listenGourdo() {
+	l, err := net.Listen("tcp", "localhost:3333")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+	fmt.Println("Listening on localhost:3333")
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go func() {
+			var p packet
+			err := binary.Read(conn, binary.LittleEndian, &p.Op)
+			if err != nil {
+				fmt.Println("Error getting op: ", err.Error())
+				return
+			}
+			if p.Op == 0 {
+				binary.Read(conn, binary.LittleEndian, &p.Data)
+			}
+			b.Send(p)
+			defer conn.Close()
+		}()
+	}
+}
+func wsServer(ws *websocket.Conn) {
+	defer ws.Close()
+	for p := range b.Listen().Ch {
+		ps, err := json.Marshal(p)
+		if err != nil {
+			fmt.Println("Error marshalling packet:", err)
+			return
+		}
+		ws.Write(ps)
+	}
+}
+
+func main() {
+	go listenGourdo()
+	http.Handle("/", websocket.Handler(wsServer))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
