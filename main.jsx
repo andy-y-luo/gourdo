c3a1c89bde2c2900701202d7f9270aa7e4dd1f8d
Lewai A <lattia87@gmail.com>
2017-04-09 02:10:48 -0400

    changed buttons and colors

--- main.jsx
+++ main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ButtonToolbar }  from 'react-bootstrap';
-import {SplitButton } from 'react-bootstrap';
+import {DropdownButton } from 'react-bootstrap';
 import {MenuItem } from 'react-bootstrap';
+import { ProgressBar } from 'react-bootstrap';
 
 var socket = new WebSocket("ws://localhost:8081");
 var button = 0;
@@ -54,23 +55,24 @@ b3(){
     return  <div>
                 <h1 id="header">Welcome to Gourdo</h1>
                 <h1>You are {this.state.Data/100} m away from an object</h1>
-                <div id="Buttons">
-                  <SplitButton title="Button 1" id="dp1"  onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
+                <ProgressBar id="pbar" max="30" bsStyle="info" now={this.state.Data/100} />
+                <ButtonToolbar id="potato">
+                  <DropdownButton bsStyle="info" title="Button 1" id="dp1" onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
                     <MenuItem eventKey="1" onClick={()=>this.b1()}>Cross the Street</MenuItem>
                     <MenuItem eventKey="2" onClick={()=>this.b1()}>Share Location</MenuItem>
                     <MenuItem eventKey="3" onClick={()=>this.b1()}>Trigger Voice Assitant</MenuItem>
-                  </SplitButton>
-                  <SplitButton title="Button 2" id="dp2"  onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
+                  </DropdownButton>
+                  <DropdownButton bsStyle="success" title="Button 2" id="dp2"  onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
                     <MenuItem eventKey="1" onClick = {()=>this.b2()}>Cross the Street</MenuItem>
                     <MenuItem eventKey="2" onClick = {()=>this.b2()}>Share Location</MenuItem>
                     <MenuItem eventKey="3" onClick = {()=>this.b2()}>Trigger Voice Assitant</MenuItem>
-                  </SplitButton>
-                  <SplitButton title="Button 3" id="dp3" onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
+                  </DropdownButton>
+                  <DropdownButton bsStyle="primary" title="Button 3" id="dp3" onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
                     <MenuItem eventKey="1" onClick = {()=>this.b3()}>Cross the Street</MenuItem>
                     <MenuItem eventKey="2" onClick = {()=>this.b3()}>Share Location</MenuItem>
                     <MenuItem eventKey="3" onClick = {()=>this.b3()}>Trigger Voice Assitant</MenuItem>
-                  </SplitButton>
-                </div>
+                  </DropdownButton>
+                </ButtonToolbar>
                 <h1>{this.state.statement}</h1>
 
             </div>
