8d602d4a198c68dd276629f1018de13b9930a968
Lewai A <lattia87@gmail.com>
2017-04-09 00:28:53 -0400

    Button Press feedback prints

--- main.jsx
+++ main.jsx
@@ -5,15 +5,15 @@ import {SplitButton } from 'react-bootstrap';
 import {MenuItem } from 'react-bootstrap';
 
 var socket = new WebSocket("ws://localhost:8081");
-
-
+var button = 0;
 
 
 class Doc extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {Data: 0}
+    this.state = {Data: 0, statement:""}
+
   }
 
   componentWillMount() {
@@ -23,27 +23,55 @@ class Doc extends React.Component {
     }
   }
 
+buttonSelect(eventKey){
+  if(eventKey == 1){
+    console.log(button)
+    console.log("Button 1 Set to Cross the Street")
+    this.setState({statement:"Button "+button+" Set to Cross the Street"})
+  }
+  if(eventKey == 2){
+    console.log(button)
+    console.log("Button 2 Set to Share Location")
+    this.setState({statement:"Button "+button+" Set to Share Location"})
+   }
+  if(eventKey == 3){
+    console.log(button)
+    console.log("Button 3 Set to Trigger Voice Assitant")
+    this.setState({statement: "Button "+button+ " Set to Trigger Voice Assitant"})
+  }
+}
+
+b1(){
+  button = 1
+}
+b2(){
+  button = 2
+}
+b3(){
+  button = 3
+}
   render() {
     return  <div>
                 <h1 id="header">Welcome to Gourdo</h1>
                 <h1>You are {this.state.Data/100} m away from an object</h1>
                 <div id="Buttons">
-                  <SplitButton title="Button 1" id="dp1" bsStyle='bs1'>
-                    <MenuItem eventKey="1">Cross the Street</MenuItem>
-                    <MenuItem eventKey="2">Share Location</MenuItem>
-                    <MenuItem eventKey="3">Trigger Voice Assitant</MenuItem>
+                  <SplitButton title="Button 1" id="dp1"  onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
+                    <MenuItem eventKey="1" onClick={()=>this.b1()}>Cross the Street</MenuItem>
+                    <MenuItem eventKey="2" onClick={()=>this.b1()}>Share Location</MenuItem>
+                    <MenuItem eventKey="3" onClick={()=>this.b1()}>Trigger Voice Assitant</MenuItem>
                   </SplitButton>
-                  <SplitButton title="Button 2" id="dp2" bsStyle='bs1'>
-                    <MenuItem eventKey="1">Cross the Street</MenuItem>
-                    <MenuItem eventKey="2">Share Location</MenuItem>
-                    <MenuItem eventKey="3">Trigger Voice Assitant</MenuItem>
+                  <SplitButton title="Button 2" id="dp2"  onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
+                    <MenuItem eventKey="1" onClick = {()=>this.b2()}>Cross the Street</MenuItem>
+                    <MenuItem eventKey="2" onClick = {()=>this.b2()}>Share Location</MenuItem>
+                    <MenuItem eventKey="3" onClick = {()=>this.b2()}>Trigger Voice Assitant</MenuItem>
                   </SplitButton>
-                  <SplitButton title="Button 3" id="dp3" bsStyle='bs1'>
-                    <MenuItem eventKey="1">Cross the Street</MenuItem>
-                    <MenuItem eventKey="2">Share Location</MenuItem>
-                    <MenuItem eventKey="3">Trigger Voice Assitant</MenuItem>
+                  <SplitButton title="Button 3" id="dp3" onSelect={(eventKey)=>this.buttonSelect(eventKey)}>
+                    <MenuItem eventKey="1" onClick = {()=>this.b3()}>Cross the Street</MenuItem>
+                    <MenuItem eventKey="2" onClick = {()=>this.b3()}>Share Location</MenuItem>
+                    <MenuItem eventKey="3" onClick = {()=>this.b3()}>Trigger Voice Assitant</MenuItem>
                   </SplitButton>
                 </div>
+                <h1>{this.state.statement}</h1>
 
             </div>
 
